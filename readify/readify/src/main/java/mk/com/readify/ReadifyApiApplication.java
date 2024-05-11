a46c6ca796bb6fac072c9c874660244ef2898309
andrioskasara <andrioska.sara@gmail.com>
2024-05-11 20:35:15 +0200

    implemented initial security setup and JWT handling

--- readify/readify/src/main/java/mk/com/readify/ReadifyApiApplication.java
+++ readify/readify/src/main/java/mk/com/readify/ReadifyApiApplication.java
@@ -1,12 +1,16 @@
 package mk.com.readify;
 
+import mk.com.readify.model.entity.Role;
+import mk.com.readify.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @EnableAsync
 public class ReadifyApiApplication {
 
@@ -14,4 +18,14 @@ public class ReadifyApiApplication {
         SpringApplication.run(ReadifyApiApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner runner(RoleRepository roleRepository) {
+        return args -> {
+            if (roleRepository.findByName("USER").isEmpty()) {
+                roleRepository.save(
+                        Role.builder().name("USER").build()
+                );
+            }
+        };
+    }
 }
