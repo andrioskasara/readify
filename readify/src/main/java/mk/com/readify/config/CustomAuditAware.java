5414520d9e009472f7b24b4b03bcdf2fcca005d9
andrioskasara <andrioska.sara@gmail.com>
2024-05-12 20:42:10 +0200

    implemented auditing feature

---
+++ readify/src/main/java/mk/com/readify/config/CustomAuditAware.java
@@ -0,0 +1,21 @@
+package mk.com.readify.config;
+
+import mk.com.readify.model.entity.User;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class CustomAuditAware implements AuditorAware<Long> {
+    @Override
+    public Optional<Long> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated() || authentication instanceof AnonymousAuthenticationToken) {
+            return Optional.empty();
+        }
+        User user = (User) authentication.getPrincipal();
+        return Optional.ofNullable(user.getId());
+    }
+}
